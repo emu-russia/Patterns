021b70dece72fd50010372be1e1d408ba6b92ac8
ogamespec <ogamespec@gmail.com>
2022-08-06 11:17:06 +0300

    Moved from psxdev repo

---
+++ patterns/textsaver.h
@@ -0,0 +1,21 @@
+// Pattern layer text saving manager.
+
+#pragma once
+
+typedef void(*TEXT_SAVER)(PatternEntry *List, int Count, char *Filename);
+
+typedef struct TextPlugin
+{
+    char    Name[0x100];
+    TEXT_SAVER Saver;
+} TextPlugin;
+
+int     TextsGetPluginNum(void);
+
+TextPlugin * TextsGetPlugin(int Index);
+
+void    TextsAddPlugin(char *Name, TEXT_SAVER Saver);
+
+void    TextsSelectPlugin(int Index);
+
+void    TextSave(char *Filename);
